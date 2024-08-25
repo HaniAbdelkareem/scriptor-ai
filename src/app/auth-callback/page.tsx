ece8b03eb9730dc8c08ca8432e5973add6f04a2b
Hani Abdelkareem <hanim1033@gmail.com>
2024-08-26 02:39:36 +0300

    tRCP setup and finishing the database sync

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,8 +2,9 @@
 
 import { useRouter, useSearchParams } from "next/navigation"
 import { trpc } from "../_trpc/client"
+import { Loader2 } from "lucide-react"
 
-const AuthCallbackPage = async () => {
+const AuthCallbackPage = () => {
   const router = useRouter()
 
   const searchParams = useSearchParams()
@@ -31,7 +32,17 @@ const AuthCallbackPage = async () => {
     router.push(origin ? `/${origin}` : "/dashboard")
   }
 
-  return <div>AuthCallbackPage</div>
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>
+          Setting up your account...
+        </h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  )
 }
 
 export default AuthCallbackPage
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,15 +1,34 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { publicProcedure, router } from "./trpc"
 import { TRPCError } from "@trpc/server"
+import { db } from "@/db"
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types"
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession()
+    // You can use "as KindeUser"
     const user = await getUser()
 
-    if (!user?.id || !user?.email) throw new TRPCError({ code: "UNAUTHORIZED" })
+    if (!user?.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" })
 
     // Check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    })
+
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      })
+    }
+
     return { success: true }
   })
 })
